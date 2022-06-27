ef17db51a32a64a31df050bdb330f6dae2e5a2a6
Madahva <galarzaguillermo.ggm@gmail.com>
2022-06-27 20:57:12 -0300

    Tab Section - JavaScript

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// Nav menu
+
 const openBtn = document.querySelector('#openBtn');
 const closeBtn = document.querySelector('#closeBtn');
 
@@ -21,3 +23,47 @@ for (var i = 0; i < modalLinks.length; i++) {
   }
 }
 
+
+
+// Tab section
+
+const firstBtn = document.querySelector('#tab-btn-1');
+const secondBtn = document.querySelector('#tab-btn-2');
+const thirdBtn = document.querySelector('#tab-btn-3');
+
+const firstTab = document.querySelector('#tab-1');
+const secondTab = document.querySelector('#tab-2');
+const thirdTab = document.querySelector('#tab-3');
+
+
+firstBtn.onclick = () => {
+  toShow(firstTab, firstBtn);
+  toHide(secondTab, secondBtn)
+  toHide(thirdTab, thirdBtn)
+}
+
+secondBtn.onclick = () => {
+  toShow(secondTab, secondBtn);
+  toHide(firstTab, firstBtn)
+  toHide(thirdTab, thirdBtn)
+}
+
+thirdBtn.onclick = () => {
+  toShow(thirdTab, thirdBtn);
+  toHide(firstTab, firstBtn);
+  toHide(secondTab, secondBtn);
+}
+
+function toShow(tab, btn) {
+  if (tab.classList.contains('features__tab--hidden')) {
+    tab.classList.remove('features__tab--hidden');
+    btn.classList.add('features__tab-buttons--active');
+  }
+}
+
+function toHide(tab, btn) {
+  if (!tab.classList.contains('features__tab--hidden')) {
+    tab.classList.add('features__tab--hidden');
+    btn.classList.remove('features__tab-buttons--active');
+  }
+}
